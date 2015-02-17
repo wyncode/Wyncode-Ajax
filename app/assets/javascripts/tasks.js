8a0d5cf30c1d8cdc3f19f60f3d582a3468b41168
Frank Ortiz <ortiz_frank@hotmail.com>
2015-02-17 09:49:06 -0500

    Final commit for breakout

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,46 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+getTasks = function(){
+	$.get('/tasks.json', function(data){
+		$('ul').text("");
+	for(var node = 0; node < data.length; node++){
+			var goal = data[node].goal;
+			var date = data[node].due_date;
+			$('ul').append("<li>"+ goal + " "+ date + "</li>")
+		}
+	});
+}
+
+postTasks = function(){
+	var text = $('#new_task').val();
+	var date = $('#new_due_date').val();
+	var path = $('#add-task').attr('action');
+	$.post( path, { 
+		task: { 
+			goal: text,
+			due_date: date
+		}
+	});
+	getTasks();
+}
+
+addTask = function(){
+	$('#add-task').on('submit', function(e){
+		e.preventDefault();
+		postTasks();
+		$("#new_task").val("");
+	});
+}
+
+runScript = function(){
+	
+	setTimeout(function(){
+		getTasks()
+	}, 10000);
+
+	addTask();
+}
+
+$(document).ready(runScript);
+$(document).on('page:load', runScript);
